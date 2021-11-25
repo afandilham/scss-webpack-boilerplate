767f686275e02e97984a0e1e96ddd40e204eec42
Moch. Ilham Afandi <afandilham@gmail.com>
2021-11-26 01:37:41 +0700

    Add src folder

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import '../assets/scss/styles.scss';
+import favicon from '../assets/img/favicon-32x32.png';
+import equilibrium from '../assets/img/image-equilibrium.jpg';
+import avatar from '../assets/img/image-avatar.png';
+
+document.getElementById('favicon').href = favicon;
+
+document.querySelector('.card-header__img').src = equilibrium;
+document.querySelector('.card-footer__img').src = avatar;
